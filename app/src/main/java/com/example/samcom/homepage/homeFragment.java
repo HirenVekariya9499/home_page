4253981d1d23034b0082e1a1c5b8cea830f3c8ae
hirenvekariya9499 <hirenvekariya0009@gmail.com>
2018-05-23 15:50:59 +0530

    recycler View Done

--- app/src/main/java/com/example/samcom/homepage/homeFragment.java
+++ app/src/main/java/com/example/samcom/homepage/homeFragment.java
@@ -2,7 +2,10 @@ package com.example.samcom.homepage;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +16,20 @@ import android.view.ViewGroup;
  */
 public class homeFragment extends Fragment {
 
+    String[] crime={"Blacklist","Crisis","Gotham","Banshee","Breaking Bad"};
 
-    public homeFragment() {
-        // Required empty public constructor
-    }
-
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_home, container, false );
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        View rootView=inflater.inflate(R.layout.fragment_home,container,false);
+
+        RecyclerView rv= (RecyclerView) rootView.findViewById(R.id.crimeRV);
+        rv.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        MyAdapter adapter=new MyAdapter(this.getActivity(),crime);
+        rv.setAdapter(adapter);
+
+        return rootView;
+    }
 }
