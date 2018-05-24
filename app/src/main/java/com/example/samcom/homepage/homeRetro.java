accc64f5bac9ed1fcc9dba0a178e6e65129fd01a
hirenvekariya9499 <hirenvekariya0009@gmail.com>
2018-05-24 16:25:21 +0530

    home fragment Dynamic..Processing

---
+++ app/src/main/java/com/example/samcom/homepage/homeRetro.java
@@ -0,0 +1,21 @@
+package com.example.samcom.homepage;
+
+public class homeRetro {
+
+    String uname;
+
+    public homeRetro(String uname, String password) {
+        this.uname = uname;
+        this.password = password;
+    }
+
+    String password;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ app/src/main/java/com/example/samcom/homepage/homeRetroAPI.java
@@ -0,0 +1,15 @@
+package com.example.samcom.homepage;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface homeRetroAPI {
+
+    String BASE_URL = "https://billcool.000webhostapp.com/samcom/";
+
+
+    @GET("login_api.php?login=login")
+    Call<List<homeRetro>> getRecommendation();
+}
